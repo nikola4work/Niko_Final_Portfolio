6d8d8a39ce04547d422748d024ef09264556f550
nikola <nikola4work@gmail.com>
2020-01-18 23:37:46 -0500

    changed profile img

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,4 +2,10 @@ $(document).ready(function() {
     $("#resume").click(function() {
         $(".resume-container").toggle(1000);
     });
+
+    // redirect user once he clicks submit button
+    $("#submit").click(function() {
+        alert("Page Under Construction - Please send me an email :)");
+        window.location.replace("http://127.0.0.1:5500/index.html");
+    });
 });
