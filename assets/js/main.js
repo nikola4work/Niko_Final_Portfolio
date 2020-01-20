0c6e78ac4701dcf97ae030161d59036fbebe8ed9
nikola <nikola4work@gmail.com>
2020-01-20 18:54:17 -0500

    added download button for resme, and live form for receving messages

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,10 +2,4 @@ $(document).ready(function() {
     $("#resume").click(function() {
         $(".resume-container").toggle(1000);
     });
-
-    // redirect user once he clicks submit button
-    $("#submit").click(function() {
-        alert("Page Under Construction - Please send me an email :)");
-        window.location.replace("http://127.0.0.1:5500/index.html");
-    });
 });
